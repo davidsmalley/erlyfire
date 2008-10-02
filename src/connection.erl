b9925a46f1fccee424f8d57e04e514af8091f3b4
David Smalley <david@davidsmalley.com>
2008-10-02 11:06:51 +0100

    Refactored state machine

--- src/connection.erl
+++ src/connection.erl
@@ -5,14 +5,14 @@
 -export([start_link/0]).
 -export([init/1, terminate/3, code_change/4, handle_event/3, handle_info/3, handle_sync_event/4]).
 -export([stop/0]).
--export([poll/2, connect/2]).
+-export([poll/2, connect/2, check_login_headers/3, close_connection/1, join_room/2]).
 
 start_link() ->
     gen_fsm:start_link({local, connection}, connection, [], []).
 
 stop() ->
     gen_fsm:send_all_state_event(connection, stop).
-    
+
 init([]) ->
     case file:consult("../conf/erlyfire.conf") of
       {ok, ConfigData} ->
@@ -34,16 +34,29 @@ connect(_Event, ConfigData) ->
     false ->
       Scheme = "http"
     end,
-  Url = [Scheme, "://", ibrowse_lib:url_encode(Domain), ".campfirenow.com/login"],
+  Url = [Scheme, "://", ibrowse_lib:url_encode(Domain), ".campfirenow.com/"],
   Post = ["email_address=", ibrowse_lib:url_encode(Username), "&", "password=", ibrowse_lib:url_encode(Password)],
-  case http:request(post, {lists:flatten(Url) , [], "application/x-www-form-urlencoded", lists:flatten(Post)}, [], []) of
-    % {ok, {{_, 302, _}, _, _}} ->
-    %   campfire:shutdown(),
-    %   stop();
-    {ok, {{_, 302, _}, _, _}} ->
+  case http:request(post, {lists:flatten(Url) ++ "login", [], "application/x-www-form-urlencoded", lists:flatten(Post)}, [], []) of
+    {ok, {{_, 302, _}, Headers, _}} ->
+      check_login_headers(Url, Headers, ConfigData);
+    _ ->
+      {stop, "Error: Campfire did not redirect us"}
+    end.
+
+join_room(_Event, ConfigData) ->
+  [{domain, Domain}, {use_ssl, Ssl}, {username, _}, {password, _}, {room_id, RoomId}] = ConfigData,
+  case Ssl of
+    true ->
+      Scheme = "https";
+    false ->
+      Scheme = "http"
+    end,
+  Url = [Scheme, "://", ibrowse_lib:url_encode(Domain), ".campfirenow.com/", "room/", RoomId],
+  case http:request(lists:flatten(Url)) of
+    {ok, {{_, 200, _}, _, _}} ->
       {next_state, poll, ConfigData};
-    {_, _} ->
-      {next_state, connect, ConfigData}
+    _ ->
+      {stop, "Error: Campfire did not return a 200"}
     end.
 
 poll(_Event, ConfigData) ->
@@ -57,19 +70,12 @@ poll(_Event, ConfigData) ->
   Url = [Scheme, "://", ibrowse_lib:url_encode(Domain), ".campfirenow.com/", "room/", Roomid, "/tabs"],
   http:request(post, {lists:flatten(Url), [], [], []}, [], []),
   {next_state, poll, ConfigData}.
-  
 
-handle_event(stop, StateName, ConfigData) ->
-  [{domain, Domain}, {use_ssl, Ssl}, _, _, {room_id, Roomid}] = ConfigData,
-  case Ssl of
-    true ->
-      Scheme = "https";
-    false ->
-      Scheme = "http"
-    end,
-  Url = [Scheme, "://", ibrowse_lib:url_encode(Domain), ".campfirenow.com/", "room/", Roomid, "/leave"],
-  http:request(post, {lists:flatten(Url)}),
-  {next_state, StateName, ConfigData}.
+handle_event(stop, _StateName, ConfigData) ->
+  {stop, "Called Shutdown", ConfigData}.
+
+% handle_event(_Event, StateName, ConfigData) ->
+%   {next_state, StateName, ConfigData}.
 
 handle_info(_Info, StateName, StateData) ->
   {next_state, StateName, StateData}.
@@ -77,11 +83,37 @@ handle_info(_Info, StateName, StateData) ->
 handle_sync_event(_Event, _From, _StateName, _StateData) ->
     ok.
 
-terminate(Reason, _State, _ConfigData) ->
+terminate(Reason, _State, ConfigData) ->
     io:format("Closing down Campfire Connection ~p", [Reason]),
+    close_connection(ConfigData),
     application:stop(inets),
     ssl:stop(),
     ok.
 
 code_change(_OldVsn, StateName, StateData, _Extra) ->
   {ok, StateName, StateData}.
+
+check_login_headers(Url, Headers, ConfigData) ->
+  Stringurl = lists:flatten(Url),
+  Loginurl = Stringurl ++ "login",
+  case lists:keysearch("location", 1, Headers) of
+    {value,{"location",Stringurl}} ->
+      {next_state, join_room, ConfigData};
+    {value,{"location",Loginurl}} ->
+      {stop, "Incorrect Login Details"};
+    false ->
+      {stop, "Error: didn't receive a location header"}
+  end.
+
+close_connection(ConfigData) ->
+  [{domain, Domain}, {use_ssl, Ssl}, _, _, {room_id, Roomid}] = ConfigData,
+  case Ssl of
+    true ->
+      Scheme = "https";
+    false ->
+      Scheme = "http"
+    end,
+  LeaveUrl = [Scheme, "://", ibrowse_lib:url_encode(Domain), ".campfirenow.com/", "room/", Roomid, "/leave"],
+  http:request(post, {lists:flatten(LeaveUrl), [], [], []}, [], []),
+  LogoutUrl = [Scheme, "://", ibrowse_lib:url_encode(Domain), ".campfirenow.com/", "logout"],
+  http:request(lists:flatten(LogoutUrl)).
